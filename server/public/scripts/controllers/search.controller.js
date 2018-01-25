48a043c48682c5cb93072d972f55a5a6c00dddbd
ernievd <ernievd@gmail.com>
2018-01-25 08:15:23 -0600

    Search works - yay!!!

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,27 +1,40 @@
-giphyApp.controller('GiphySearchController', ['$http', function($http){
+giphyApp.controller('GiphySearchController', ['GifService','$http', function(GifService, $http){
     let self = this;
+    //This is the output of the final needed url from the services search get
+    self.searchData = GifService.gifSearch;
+	console.log('Self.SearchData is ', self.searchData);
 
-    self.searchText="";
+	//**** Do I have to first declare it here before I use it? How do I get it from the DOM to here?
+    //self.searchTxt = '';
+	console.log('self.searchtext is :' , self.searchTxt);
+	// **** Assign the search string to a variable in the gifservice DOES NOT WORK - How do we get info from the DOM?
+    GifService.searchString = self.searchTxt;
 
-    self.showImage = function(){
-        const config = {
-            params:{
-                api_key: 'wR50GyIW22ZM02cGq378wMWBg3FUResr',
-                q: self.searchText
-            }
-        };
-        console.log('config of Search is ',  config);
-        $http.get('https://api.giphy.com/v1/stickers/search', config).then(function(response){
-            console.log('search response ', response);
-            console.log(response.data.data[0].url);
-            self.GifUrl = response.data.data[0].images.fixed_height.url;
-            console.log('search total response is ', self.GifUrl );
+	self.showImage = function(searchTxt){
+        GifService.searchGif(searchTxt)
+    }
 
-        });
-        self.showFlag = false;
-        self.showFlag = true;
-        console.log(self.showFlag);
-    };
-    console.log('end of search controller');
+    // self.searchText="";
+    //
+    // self.showImage = function(){
+    //     const config = {
+    //         params:{
+    //             api_key: 'wR50GyIW22ZM02cGq378wMWBg3FUResr',
+    //             q: self.searchText
+    //         }
+    //     };
+    //     console.log('config of Search is ',  config);
+    //     $http.get('https://api.giphy.com/v1/stickers/search', config).then(function(response){
+    //         console.log('search response ', response);
+    //         console.log(response.data.data[0].url);
+    //         self.GifUrl = response.data.data[0].images.fixed_height.url;
+    //         console.log('search total response is ', self.GifUrl );
+    //
+    //     });
+    //     self.showFlag = false;
+    //     self.showFlag = true;
+    //     console.log(self.showFlag);
+    // };
+    // console.log('end of search controller');
 }]); // End of search controller
 
--- server/public/scripts/controllers/services/gif.service.js
+++ server/public/scripts/controllers/services/gif.service.js
@@ -4,34 +4,47 @@ giphyApp.service('GifService',[ '$http', function($http) {
 	var self = this;
 
 	let apiKey = 'wR50GyIW22ZM02cGq378wMWBg3FUResr';
-	let url = 'https://api.giphy.com/v1/gifs/random';
-
+	let randomUrl = 'https://api.giphy.com/v1/gifs/random';
 
 	self.greeting = 'Greetings from the Gif service';
 
 	self.gifLocation = {};
 
 	self.getRandom = function () {
-		console.log('in service');
+		console.log('in getRandom service');
 		const config = {
 			params:{
 				api_key: apiKey
 			}
 		};
-		$http.get(url, config).then(function(response){
+		$http.get(randomUrl, config).then(function(response){
 			console.log('random service response ', response);
 			self.gifLocation.imageUrl = response.data.data.image_url;
 			// console.log('random service image final link ', self.GifUrl);
 			//self.GifImage = response.
 		});
-
 		self.showFlag = false;
-
 		self.showImage = function(){
 			self.showFlag = true;
 			console.log(self.showFlag);
 		};
+	}; // End self.getRandom function
 
-	}
+	self.gifSearch = {};
+	self.searchGif = function(search){
+		console.log('in searchGif service function');
+		const config = {
+			params:{
+				api_key: apiKey,
+				q: search
+			}
+		}
+		$http.get('https://api.giphy.com/v1/stickers/search', config).then(function(response){
+			console.log('search response ', response);
+			console.log(response.data.data[0].url);
+			self.gifSearch.gifUrl = response.data.data[0].images.fixed_height.url;
+			console.log('search total response is ', self.GifUrl );
 
+		});
+	};// End self.searchGif function
 }]);
